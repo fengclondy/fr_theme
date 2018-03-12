7138192b9d0c16311c10575cab2b073fdb3d5724
Tom <344548940@qq.com>
2018-03-12 09:48:39 +0800

    添加模板页面的配置

--- qingsuan_finreport/finereport-master/src/com/qdch/core/Constants.java
+++ qingsuan_finreport/finereport-master/src/com/qdch/core/Constants.java
@@ -8,5 +8,10 @@ public class Constants {
 	
 	//清算所gp库hub用户
 	public static final String QSS_GP_HUB = "qss_gp_hub";
+	
+	//layout模板路径
+	public static final String LAYOUT_TEMPLTE_DIR = "/WEB-INF/pages/layout/";
+	
+	public static final String ATTR_TEMPLTE_DIR = "tpl_dir";
 
 }
