d3e49c55074ad9dbb1466c3a06296a17a6b34c20
penguinmyac <3550928@qq.com>
2018-04-04 18:30:22 +0800

    完善代码

--- qingsuan_finreport/finereport-master/src/com/qdch/config/Config.java
+++ qingsuan_finreport/finereport-master/src/com/qdch/config/Config.java
@@ -216,6 +216,6 @@ public class Config extends JFinalConfig {
 	}
 
 	public static void main(String[] args){
-		JFinal.start("WebRoot", 8090, "/", 5);
+		JFinal.start("WebRoot", 8080, "/", 5);
 	}
 }
