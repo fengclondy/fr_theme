aa17e06cd4a6b05745ae638419e50c8c58b2893d
tangmumu <2679167174@qq.com>
2018-04-10 19:15:07 +0800

    p2p接口对接

--- qingsuan_finreport/finereport-master/src/com/qdch/p2p/controller/BorrowerController.java
+++ qingsuan_finreport/finereport-master/src/com/qdch/p2p/controller/BorrowerController.java
@@ -1,9 +1,17 @@
 package com.qdch.p2p.controller;
 
+import java.util.ArrayList;
 import java.util.List;
 
+import org.apache.commons.lang.StringUtils;
+
+import com.jfinal.plugin.activerecord.Record;
 import com.qdch.core.BaseController;
-import com.qdch.p2p.model.BorrowerModel;
+import com.qdch.p2p.model.DefenInfoModel;
+import com.qdch.p2p.model.WorkInfoModel;
+import com.qdch.p2p.model.XinYongInfoModel;
+import com.qdch.p2p.model.ZiChanInfoModel;
+import com.qdch.p2p.model.ZiRanRenJiChuInfoMoDel;
 import com.qdch.xd.model.JyscModel;
 import com.qdch.xd.model.KeyIndicatorsModel;
 /**
@@ -15,88 +23,38 @@ import com.qdch.xd.model.KeyIndicatorsModel;
 public class BorrowerController extends BaseController {
 	/**
 	 * 
-	 * @author lixiaoyi
-	 * @date 2018年4月8日
+	 * @author 高照
+	 * @date 2018年4月10日
 	 * @TODO 借款人总览首页
 	 */
 	public void index(){
-		setAttr("pingtailist", BorrowerModel.dao.getScoreList(getDataScopeByUserNameForP2p()));
-		setAttr("ziRanRenInfo",BorrowerModel.dao.getBasicinfo(getDataScopeByUserNameForP2p()));
+		setAttr("pingtailist", DefenInfoModel.dao.getScoreList(getDataScopeByUserNameForP2p(),null));
+		//setAttr("ziRanRenInfo",ZiRanRenJiChuInfoMoDel.dao.getBasicinfo(getDataScopeByUserNameForP2p()));
 		render("p2p/pages/04_05jiekuanrenhuaxiang.html");
 	}
 	
+	public void getBasicinfo(){
+		String ptType=getPara("jys");
+		String hasInfo=getPara("hasInfo");
+		List<DefenInfoModel> ptList=new ArrayList<DefenInfoModel>();
+		
+		//查询全部
+		ptList=DefenInfoModel.dao.getScoreList(getDataScopeByUserNameForP2p(),ptType);
+		for(DefenInfoModel model:ptList){
+			ZiRanRenJiChuInfoMoDel zrr=ZiRanRenJiChuInfoMoDel.dao.getBasicinfo(getDataScopeByUserNameForP2p(),model.getStr("jysc"),hasInfo);
+			model.put("zrr", zrr);
+			XinYongInfoModel xinyon=XinYongInfoModel.dao.getCreditinfo(getDataScopeByUserNameForP2p(),model.getStr("jysc"),hasInfo);
+			model.put("xinyon",xinyon);
+			ZiChanInfoModel zichan=ZiChanInfoModel.dao.getAssetinfo(getDataScopeByUserNameForP2p(),model.getStr("jysc"),hasInfo);
+			model.put("zichan",zichan);
+			WorkInfoModel job=WorkInfoModel.dao.getJobinfo(getDataScopeByUserNameForP2p(),model.getStr("jysc"),hasInfo);
+			model.put("job",job);
+		}
+		mRenderJson(ptList);
+	}
 	
 	
 	
-	/**
-	 * 
-	 * @author gaozhao
-	 * @date 2018年4月9日
-	 * @TODO 获取自然人基本信息
-	 */
-	public void getZiRanRenInfo(){
-		List<BorrowerModel> borrowerZiRanRenInfo=BorrowerModel.dao.getBasicinfo(getDataScopeByUserNameForP2p());
-		mRenderJson(borrowerZiRanRenInfo);
-	}
-	/**
-	 * 
-	 * @author gaozhao
-	 * @date 2018年4月9日
-	 * @TODO 获取自然人信用信息
-	 */
-	public void getZiRanRenXinYong(){
-		List<BorrowerModel> borrowerZiRanRenXinyong=BorrowerModel.dao.getCreditinfo(getDataScopeByUserNameForP2p());
-		mRenderJson(borrowerZiRanRenXinyong);
-	}
-	/**
-	 * 
-	 * @author gaozhao
-	 * @date 2018年4月9日
-	 * @TODO 获取自然人资产信息
-	 */
-	public void getZiRanRenZiChanInfo(){
-		List<BorrowerModel>  borrowerZiRanRenZiChanInfo=BorrowerModel.dao.getAssetinfo(getDataScopeByUserNameForP2p());
-		mRenderJson(borrowerZiRanRenZiChanInfo);
-	}
-	/**
-	 * 
-	 * @author gaozhao
-	 * @date 2018年4月9日
-	 * @TODO 获取自然人工作信息
-	 */
-	public void getZiRanRenWorkInfo(){
-		List<BorrowerModel>  borrowerZiRanRenWorkInfo=BorrowerModel.dao.getJobinfo(getDataScopeByUserNameForP2p());
-		mRenderJson(borrowerZiRanRenWorkInfo);
-	}
-	/**
-	 * 
-	 * @author gaozhao
-	 * @date 2018年4月9日
-	 * @TODO 获取企业基本信息
-	 */
-	public void getQiYeInfo(){
-		 List<BorrowerModel> borrowerQiYeInfo=BorrowerModel.dao.getCompybasicinfo(getDataScopeByUserNameForP2p());
-		 mRenderJson(borrowerQiYeInfo);
-	}
-	/**
-	 * 
-	 * @author gaozhao
-	 * @date 2018年4月9日
-	 * @TODO 获取企业其它信息
-	 */
-	public void getQiYeQiTaInFo(){
-		 List<BorrowerModel>  borrowerQiYeQiTaInfo=BorrowerModel.dao.getOtherinfo(getDataScopeByUserNameForP2p());
-		 mRenderJson(borrowerQiYeQiTaInfo);
-	}
-	/**
-	 * 
-	 * @author gaozhao
-	 * @date 2018年4月10日
-	 * @TODO 获取自然人基本信息
-	 */
 	
-	public void getPingTai(String pingtainame){
-		List<BorrowerModel> borrowerZiRanRenInfo=BorrowerModel.dao.getPingtai(getDataScopeByUserNameForP2p(),pingtainame);
-		mRenderJson(borrowerZiRanRenInfo);
-	}
+	
 }
