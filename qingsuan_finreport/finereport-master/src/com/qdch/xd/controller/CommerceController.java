26ada8712b6cf075fba04558b298dcb60a7f7b88
lixiaoyiyi <xiaoyi.li@hlsofttech.com>
2018-04-25 13:06:06 +0800

    小贷管理风险跳转

--- qingsuan_finreport/finereport-master/src/com/qdch/xd/controller/CommerceController.java
+++ qingsuan_finreport/finereport-master/src/com/qdch/xd/controller/CommerceController.java
@@ -46,7 +46,7 @@ public class CommerceController extends BaseController{
 	public void index(){
 		String name=getPara("name");
 		if (name==null||"".equals(name)) {
-			name="青岛鲁金所股权投资基金有限公司";
+			name="重庆海尔小额贷款有限公司";
 		}
 		setAttr("company", name);
 	   /* List<CompanysInfoModel> info = CompanysInfoModel.dao.getInfo(getDataScopeByUserName(),name);
