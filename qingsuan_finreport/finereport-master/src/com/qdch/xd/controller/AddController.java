04aeef706ed3a4d105a878716541bebb6bdd1f06
lixiaoyiyi <xiaoyi.li@hlsofttech.com>
2018-05-04 10:38:14 +0800

    交易量新增

---
+++ qingsuan_finreport/finereport-master/src/com/qdch/xd/controller/AddController.java
@@ -0,0 +1,16 @@
+package com.qdch.xd.controller;
+
+import com.qdch.core.BaseController;
+/**
+ * 交易量
+ * @author lixiaoyi
+ * @date 2018年5月4日上午10:33:52
+ * @TODO
+ */
+public class AddController extends BaseController{
+
+	public void index(){
+		
+		render("xd/pages/08_02_01jiaoyiliangAdd.html");
+	}
+}
